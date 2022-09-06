233be8b369263fa4b565de6388d111c2330c944c
Andrii Nahornyi <navamster@gmail.com>
2022-09-06 18:15:11 +0300

    Update task-04

--- js/task-04.js
+++ js/task-04.js
@@ -7,3 +7,25 @@
 // Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
+
+const valueCounterEl = document.querySelector('#value');
+// console.log('valueCounterEl', valueCounterEl);
+const decrementBtnEl = document.querySelector('[data-action="decrement"]');
+// console.log('decrementBtnEl', decrementBtnEl);
+const incrementBtnEl = document.querySelector('[data-action="increment"]');
+// console.log('incrementBtnEl', incrementBtnEl);
+decrementBtnEl.addEventListener('click', targetDecrementBtnClickHandler);
+// console.log('targetDecrementBtn', decrementBtnEl);
+incrementBtnEl.addEventListener('click', targetIncrementBtnClickHandler);
+// console.log('targetIncrementBtn', incrementBtnEl);
+let counterValue = 0;
+function targetDecrementBtnClickHandler() {
+    counterValue -= 1;
+    valueCounterEl.textContent = counterValue;
+    // console.log(valueCounterEl);
+}
+function targetIncrementBtnClickHandler() {
+    counterValue += 1;
+    valueCounterEl.textContent = counterValue;
+    // console.log(valueCounterEl);
+}
