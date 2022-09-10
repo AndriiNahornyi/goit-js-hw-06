52cdb67305a74b1300b1f7bf6c0f88dde183f82e
Andrii Nahornyi <navamster@gmail.com>
2022-09-10 14:17:46 +0300

    adds task-10 solution

--- js/task-10.js
+++ js/task-10.js
@@ -12,5 +12,57 @@
 // Створи функцію destroyBoxes(), яка очищає вміст div#boxes, у такий спосіб видаляючи всі створені елементи.
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+const refs = {
+  input: document.querySelector("[type='number']"),
+  btnCreate: document.querySelector('[data-create]'),
+  div: document.querySelector('#controls'),
+  divBoxes: document.querySelector('#boxes'),
+  btnDestroy: document.querySelector('[data-destroy]'),
+}
+refs.div.addEventListener('click', onClickHandler);
+function onClickHandler(event) {
+  const btnEvent = event.target.dataset;
+  if (btnEvent.hasOwnProperty('create')) {
+    targetInputHandler();
+  } else if (btnEvent.hasOwnProperty('destroy')) {
+    targetClickHandler();
+  } else {
+    return;
+  } 
+ // console.dir(btnEvent);
+}
+// refs.btnCreate.addEventListener('click', targetInputHandler);
+// refs.btnDestroy.addEventListener('click', targetClickHandler);
+
+function createBoxes(amount) {
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = size + "px";
+    box.style.height = size + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    refs.divBoxes.append(box);
+    size += 10;
+  }
+}
+
+function targetInputHandler() {
+  createBoxes(refs.input.value);
+  refs.input.value = '';
+}
+
+function targetClickHandler() {
+  const {value} = refs.input;
+  if (!value || value < 0) {
+    refs.divBoxes.innerHTML = '';
+    return;
+  }
+  for (let i = 0; i < value; i++) {
+    if (i > refs.divBoxes.childElementCount - 1) {
+      return;
+    }
+    refs.divBoxes.lastElementChild.remove();
+  }
 }
